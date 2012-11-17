939337c18051a8887245314eab0a756544588c18
Didii Theodor-Cosmin <cosmin.didii@gmail.com>
2012-11-17 14:01:25 -0800

    Added eyes and mouth

--- TheImpossibleGame/TheImpossibleGame/js/classes/player.js
+++ TheImpossibleGame/TheImpossibleGame/js/classes/player.js
@@ -1,10 +1,9 @@
 function Player (size, color) {
-  if (size === undefined) { size = 40; }
+  if (size === undefined) { size = 50; }
   if (color === undefined) { color = "#ff0000"; }
   this.x = 0;
   this.y = 0;
   this.size = size;
-  this.rotation = 0;
   this.scaleX = 1;
   this.scaleY = 1;
   this.color = utils.parseColor(color);
@@ -14,16 +13,29 @@ function Player (size, color) {
 Player.prototype.draw = function (context) {
   context.save();
   context.translate(this.x, this.y);
-  context.rotate(this.rotation);
-  context.scale(this.scaleX, this.scaleY);
+//  context.scale(this.scaleX, this.scaleY);
   
   context.lineWidth = this.lineWidth;
   context.fillStyle = this.color;
   context.beginPath();
-  //x, y, radius, start_angle, end_angle, anti-clockwise
+    //x, y, radius, start_angle, end_angle, anti-clockwise
+
   context.rect(0, 0, this.size, this.size);
   context.closePath();
   context.fill();
+  
+  context.beginPath();
+  context.arc(25, 20, 20, 0, 1 * Math.PI);
+  context.stroke();
+
+  context.beginPath();
+  context.arc(15, 10, 10, 0, 2 * Math.PI);
+  context.stroke();
+
+  context.beginPath();
+  context.arc(35, 10, 10, 0, 2 * Math.PI);
+  context.stroke();
+
   if (this.lineWidth > 0) {
     context.stroke();
   }
