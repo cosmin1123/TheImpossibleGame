183a4499e26cf8b143109bb32f7fcf5e9bf382da
Didii Theodor-Cosmin <cosmin.didii@gmail.com>
2012-11-17 13:20:31 -0800

    Added player size and reposition the player start
    Bug, player teleports at the begining

--- TheImpossibleGame/TheImpossibleGame/js/classes/player.js
+++ TheImpossibleGame/TheImpossibleGame/js/classes/player.js
@@ -1,9 +1,9 @@
-function Player (radius, color) {
-  if (radius === undefined) { radius = 40; }
+function Player (size, color) {
+  if (size === undefined) { size = 40; }
   if (color === undefined) { color = "#ff0000"; }
   this.x = 0;
   this.y = 0;
-  this.radius = radius;
+  this.size = size;
   this.rotation = 0;
   this.scaleX = 1;
   this.scaleY = 1;
@@ -21,7 +21,7 @@ Player.prototype.draw = function (context) {
   context.fillStyle = this.color;
   context.beginPath();
   //x, y, radius, start_angle, end_angle, anti-clockwise
-  context.rect(0, 0, 40,40);
+  context.rect(0, 0, this.size, this.size);
   context.closePath();
   context.fill();
   if (this.lineWidth > 0) {
