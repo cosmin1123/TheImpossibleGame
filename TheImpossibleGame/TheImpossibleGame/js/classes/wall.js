118cf73edbe8951642c3bab39aff13a18c4efadd
Ramascanu Catalin <catalin.rmc@gmail.com>
2012-11-23 21:11:06 +0200

    Added wall

---
+++ TheImpossibleGame/TheImpossibleGame/js/classes/wall.js
@@ -0,0 +1,27 @@
+﻿function Wall(size, posX, posY) {
+    if (size === undefined) { size = 50; }
+
+    this.x = posX;
+    this.y = posY;
+    this.width = size;
+    this.height = size;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.lineWidth = 1;
+}
+
+Wall.prototype.draw = function (context) {
+
+
+    context.save();
+    context.translate(this.x, this.y);
+    context.scale(this.scaleX, this.scaleY);
+
+    var img = document.getElementById("wall");
+    img.height = this.height;
+    img.width = this.width;
+
+    context.drawImage(img, this.x, this.y);
+
+    context.restore();
+};
