7150ddfefaed6e7a170824e405c20b4a751d057e
Ramascanu Catalin <catalin.rmc@gmail.com>
2012-12-16 10:24:44 +0200

    Modified movement

--- TheImpossibleGame/TheImpossibleGame/js/classes/player.js
+++ TheImpossibleGame/TheImpossibleGame/js/classes/player.js
@@ -6,27 +6,25 @@ function Player (id,width, height,posX,posY) {
   this.height = height;
   this.startX = posX;
   this.startY = posY;
+  
 }
 
 Player.prototype.move = function () {
-   
-    if (vx < 4 && vx > -4)
-        vx += ax;
-    if (vy < 4 && vy > -4)
-        vy += ay;
-        
-    context.clearRect(this.x, this.y, this.width, this.height); 
- 
-    player.XBefore = player.x;
-    player.YBefore = player.y;
-    
-    player.x += vx;
-    player.y += vy;
-    
- 	 wall_collission();
- 	     
-    context.save();
-    context.drawImage(this.img, this.x, this.y,this.width,this.height);
+    context.clearRect(this.x, this.y, this.width, this.height);
+    this.XBefore = this.x;
+    this.YBefore = this.y;
+    if (isRight == true)
+        this.x += 4;
+    if (isLeft == true)
+        this.x -= 4;
+    if (isUp == true)
+        this.y -= 4;
+    if (isDown == true)
+        this.y += 4;
+
+    wall_collission();
+ 	context.save();
+ 	context.drawImage(this.img, this.x, this.y, this.width, this.height);
     context.restore();
 
 }
@@ -34,11 +32,11 @@ Player.prototype.move = function () {
 wall_collission = function(){
 	
 	for( var i = 0; i < index; i++)	
-		if(!(player.x + player.width < walls[i].x ||
-			walls[i].x + walls[i].width < player.x ||
+         if (!(player.x + player.width < walls[i].x ||
+            walls[i].x + walls[i].width < player.x ||
 			player.y + player.height < walls[i].y ||
 			walls[i].y + walls[i].height  < player.y)) {
  			 			player.x = player.XBefore;
-            		player.y = player.YBefore;
+            		    player.y = player.YBefore;
        }
 }
